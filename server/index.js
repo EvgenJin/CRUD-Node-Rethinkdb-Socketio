4eb992f44ea613b9bf56bb1b48443e85e056a389
Evgen <rumbiroid@gmail.com>
2018-03-14 21:41:11 +0500

    1

---
+++ server/index.js
@@ -0,0 +1,76 @@
+const http = require('http');
+const socketIO = require('socket.io');
+const r = require('rethinkdb');
+const config = require('./config.json');
+
+const db = Object.assign(config.rethinkdb, {  
+    db: 'timeline'
+});
+// const server = http.Server(app);  
+const server = http.createServer(function(request, response){});
+const io = socketIO(server);
+
+r.connect(db)  
+    .then(conn => {
+        // обновления
+        r.table('messages')
+            .changes()
+            .run(conn)
+            .then(cursor => {
+                cursor.each((err, data) => {
+                    const message = data.new_val;
+                    io.sockets.emit('messages', message);
+                });
+            });
+
+        // показать все записи
+        io.on('connection', (client) => {
+            r.table('messages')
+                .run(conn)
+                .then(cursor => {
+                    cursor.each((err, message) => {
+                        io.sockets.emit('messages', message);
+                    });
+                });
+            // добавить запись
+            client.on('/messages', (body) => {
+                const {
+                    name, message
+                } = body;
+                const data = {
+                    name, message, date: new Date()
+                };
+                r.table('messages').insert(data).run(conn);
+            });
+        });
+        // отладка
+        io.on('connection', function(socket){
+            socket.emit('stream', {
+                    'title':'первое',
+                    'text':'второе'
+            });
+        });        
+
+        // server.listen(8000, () => console.log('Заходи на localhost:8000'));
+
+        server.listen(8000);
+        console.log('ready on localhost:8000');
+        
+        // use socket.io
+        // var io = require('socket.io').listen(server);
+        // console.log('socket on localhost:8000');
+
+        io.on('connection', function (socket) {
+            var now = new Date()
+            console.log('A client is connected!' + now);   
+        });
+
+
+
+    })
+
+
+
+
+
+
