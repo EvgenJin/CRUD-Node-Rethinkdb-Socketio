f0b0803d2c84b45d1d3beb6c127a6e5927a25278
Evgen <rumbiroid@gmail.com>
2018-04-09 17:06:41 +0500

    1

--- server/index.js
+++ server/index.js
@@ -13,16 +13,16 @@ const io = socketIO(server);
 r.connect(db)  
     .then(conn => {
         // обновления не включать
-        r.table('messages')
-            .changes()
-            .run(conn)
-            .then(cursor => {
-                cursor.each((err, data) => {
-                    const message = data.new_val
-                    io.sockets.emit('changes', message)
-                    console.log(message)
-                });
-            });
+        // r.table('messages')
+        //     .changes()
+        //     .run(conn)
+        //     .then(cursor => {
+        //         cursor.each((err, data) => {
+        //             const message = data.new_val
+        //             io.sockets.emit('changes', message)
+        //             console.log(message)
+        //         });
+        //     });
         io.on('connection', (client) => {
             // добавить запись
             client.on('messages', (body) => {
@@ -41,6 +41,14 @@ r.connect(db)
                 .run(conn)
                 console.log(id)
             }),
+
+             r.table('messages')
+            .run(conn)
+            .then(cursor => {
+                cursor.toArray((err, message) => {
+                    console.log(message)
+                });
+            });
 
             client.on('test',(dates) => {
                 const {
@@ -70,6 +78,40 @@ r.connect(db)
         // use socket.io
         // var io = require('socket.io').listen(server);
         // console.log('socket on localhost:8000');
+/*
+        <!DOCTYPE html>
+        <html>
+        <body>
+        
+        <p>Click the button to get the square root of each element in the array.</p>
+        
+        <button onclick="myFunction()">Try it</button>
+        
+        <p id="demo"></p>
+        <p id="demo1"></p>
+        
+        <script>
+        var numbers = [{date:'01.01.2018',message:'1'},{date:'01.02.2018',message:'2'}];
+        
+        var task_names = numbers.map(function (number, index, array) {
+             return number.date;
+        });
+        var task_message = numbers.map(function (number, index, array) {
+             return number.message;
+        });
+        
+        function myFunction() {
+            x = document.getElementById("demo")
+            x.innerHTML = task_names
+            y = document.getElementById("demo1")
+            y.innerHTML = task_message
+        }
+        
+        </script>
+        
+        </body>
+        </html>
+*/
 
         io.on('connection', function (socket) {
             console.log('A client is connected!');   
